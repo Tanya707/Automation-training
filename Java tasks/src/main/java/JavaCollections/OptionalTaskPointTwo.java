fab51aa11125aaff47806d0691283300971f830a
Tatyana Borbitskaya <tanborb@tut.by>
2020-06-06 22:55:30 +0300

    Add OptionalTaskPointTwo to Java Collections

---
+++ Java tasks/src/main/java/JavaCollections/OptionalTaskPointTwo.java
@@ -0,0 +1,20 @@
+package JavaCollections;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class OptionalTaskPointTwo {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Inter a number:");
+        Integer input = in.nextInt();
+        Stack<Integer> result = new Stack<Integer>();
+        while (input != 0) {
+            result.push(input % (10));
+            input = input / 10;
+            System.out.print(result.pop());
+        }
+    }
+}
+
---
+++ Java tasks/src/main/java/MainTask/MilitaryPlane.java
@@ -0,0 +1,29 @@
+package MainTask;
+
+public class MilitaryPlane extends Plane{
+
+    private int maxSpeed;
+    private int maxLoadCapacity;
+    private int passengersCapacity;
+
+    public MilitaryPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity, double
+            fuelConsumption) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.maxSpeed = maxSpeed;
+        this.maxLoadCapacity = maxLoadCapacity;
+        this.passengersCapacity = passengersCapacity;
+      }
+
+      public int getMaxSpeed() {
+        return maxSpeed;
+       }
+
+       public int getMaxLoadCapacity() {
+        return maxLoadCapacity;
+       }
+
+       public int getPassengersCapacity() {
+        return passengersCapacity;
+       }
+    }
+
